81b6f5a57aae5c94eecc08c55e7a69587eebd6aa
glebkalachev <megas781@gmail.com>
2019-07-11 23:12:22 +0400

    Natasha added

--- src/index/index.js
+++ src/index/index.js
@@ -5,17 +5,17 @@ let onscroll = function () {
     let heroHeight = parseInt(getComputedStyle(document.querySelector('.hero')).height);
     let scroll = window.scrollY;
 
-    // let scrollPastHeroRatio = (scroll / heroHeight);
+    let scrollPastHeroRatio = (scroll / heroHeight);
 
     //Затемнение слайдшоу при скроллинге
-    // document.querySelector('.slider').style.opacity = 1 - scrollPastHeroRatio * 1.2;
+    document.querySelector('.slider').style.opacity = 1 - scrollPastHeroRatio * 1.2;
 
-    // //Скрывание шапки после скролла слайдшоу
-    // if (scrollPastHeroRatio > 0.7) {
-    //     document.querySelector('.header').classList.add('hidden');
-    // } else {
-    //     document.querySelector('.header').classList.remove('hidden');
-    // }
+    //Скрывание шапки после скролла слайдшоу
+    if (scrollPastHeroRatio > 0.7) {
+        document.querySelector('.header').classList.add('hidden');
+    } else {
+        document.querySelector('.header').classList.remove('hidden');
+    }
 
     // //Замедление скроллинга текста (параллакс)
     document.querySelectorAll('.slide__text-container').forEach(function (container) {
