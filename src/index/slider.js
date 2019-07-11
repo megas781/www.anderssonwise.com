531d3b2c8685714d7bf3c4c8854370c7e581661d
glebkalachev <megas781@gmail.com>
2019-07-11 22:04:23 +0400

    animation changing implemented

--- src/index/slider.js
+++ src/index/slider.js
@@ -20,66 +20,77 @@ buttons[2].addEventListener('click', function () {
     })
 })
 
-// TweenLite.from('.slider__dots', 1, {
-//     opacity: 0,
-//     x: 50,
-//     delay: 0.5
-// }, 'secondStep');
-//
-//
-// // Анимирование переключения слайдов
-// function startSlidingMechanism() {
-//     let slides = document.querySelectorAll('.slider__slide');
-//     let slideIndex = 0;
-//     let activeSlide = slides[slideIndex];
-//     let dots = document.querySelectorAll('.slider .dot');
-//     dots[slideIndex].classList.add('active');
-//
-//     function animateSlide(slide) {
-//         slide.classList.add('slider__slide_active');
-//         let slideImage = slide.querySelector('.slide__image');
-//         let slideTextContainer = slide.querySelector('.slide__text-container');
-//         let slideTitle = slide.querySelector('.slide__title');
-//         let slideSubtitle = slide.querySelector('.slide__subtitle');
-//
-//         let recursiveAnimation = new TimelineLite();
-//
-//         recursiveAnimation
-//             .fromTo(slideImage, 1, {opacity: 0},{opacity: 1})
-//             .addLabel('imageAppeared')
-//             .fromTo(slideTitle, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0})
-//             .fromTo(slideSubtitle, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0})
-//             .fromTo(slideImage, 7, {scale: 1},{scale: 1.1, ease: Power0.easeNone}, 'imageAppeared-=0.5')
-//
-//             .addLabel('hide', '-=2')
-//             .to(slideTextContainer, 1, {opacity: 0, y: -50}, 'hide')
-//             .to(slideImage, 2, {
-//                 opacity: 0,
-//                 onStart() {
-//
-//                     //До того, как индекс обновился, деактивируем текущую точку
-//                     dots[slideIndex].classList.remove('active');
-//
-//                     slideIndex += 1;
-//                     if (slideIndex == slides.length) {
-//                         slideIndex = 0;
-//                     }
-//                     animateSlide(slides[slideIndex]);
-//                     //После того, как индекс обновился, активируем новую точку
-//                     dots[slideIndex].classList.add('active');
-//                 },
-//                 onComplete() {
-//                     slide.classList.remove('slider__slide_active');
-//                 }
-//             }, 'hide')
-//             .set(slideTextContainer, {opacity: 1, y: 0})
-//
-//     }
-//
-//     animateSlide(activeSlide);
-// }
-//
-// startSlidingMechanism();
+TweenLite.from('.slider__dots', 1, {
+    opacity: 0,
+    x: 50,
+    delay: 0.5
+}, 'secondStep');
+
+
+// Анимирование переключения слайдов
+function startSlidingMechanism() {
+
+    let slides = document.querySelectorAll('.slider__slide');
+
+    let currentSlideIndex = 0;
+    let nextSlideIndex = 1;
+    let currentSlide = slides[currentSlideIndex];
+
+    let dots = document.querySelectorAll('.slider .dot');
+    dots[currentSlideIndex].classList.add('active');
+
+    function animateSlide(slide) {
+
+        let slideImage = slide.querySelector('.slide__image');
+        let slideTextContainer = slide.querySelector('.slide__text-container');
+        let slideTitle = slide.querySelector('.slide__title');
+        let slideSubtitle = slide.querySelector('.slide__subtitle');
+
+        let recursiveAnimation = new TimelineLite();
+        recursiveAnimation.delay = 1;
+
+        recursiveAnimation
+            .fromTo(slideTitle, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0})
+            .fromTo(slideSubtitle, 1, {opacity: 0, y: 50}, {opacity: 1, y: 0})
+            .fromTo(slideImage, 7, {scale: 1}, {scale: 1.1, ease: Power0.easeNone}, 0)
+
+            .addLabel('hide', '-=1')
+            .to(slideTextContainer, 1, {y: -50}, 'hide')
+            .to(slide, 1, {
+                opacity: 0,
+                onStart() {
+
+                    //До того, как индекс обновился, деактивируем текущую точку
+                    dots[currentSlideIndex].classList.remove('active');
+
+                    //После того, как индекс обновился, активируем новую точку
+                    dots[(currentSlideIndex + 1) % slides.length].classList.add('active');
+
+                    console.log(`вызов анимации слайда по индексу ${(currentSlideIndex + 1) % slides.length}`);
+                    animateSlide(slides[(currentSlideIndex + 1) % slides.length]);
+                },
+                onComplete() {
+
+                    slides[currentSlideIndex].classList.remove('current');
+
+                    // Делаем смещение индексов
+                    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+                    nextSlideIndex = (nextSlideIndex + 1) % slides.length;
+
+                    slides[currentSlideIndex].classList.remove('next');
+                    slides[currentSlideIndex].classList.add('current');
+                    slides[nextSlideIndex].classList.add('next');
+                }
+            }, 'hide')
+            .set(slideTextContainer, {y: 0})
+            .set(slide, {opacity: 1})
+
+    }
+
+    animateSlide(currentSlide);
+}
+
+startSlidingMechanism();
 
 
 
